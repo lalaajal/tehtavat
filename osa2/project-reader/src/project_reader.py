cf4bfd13a75fe4870a26f7f4440cff67167fa20e
lalaajal <lauri.j.laajala@student.jyu.fi>
2024-11-10 20:58:09 +0200

    8. tehtävään asti

--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import tomli
 
 
 class ProjectReader:
@@ -9,7 +10,10 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        data = tomli.loads(content)
+        return Project(data["tool"]["poetry"]["name"], data["tool"]["poetry"]["description"],
+            data["tool"]["poetry"]["license"],
+            data["tool"]["poetry"]["authors"],
+            data["tool"]["poetry"]["dependencies"],
+            data["tool"]["poetry"]["group"]["dev"]["dependencies"])
 
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
